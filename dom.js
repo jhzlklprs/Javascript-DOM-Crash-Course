5e4436b077f5314ef570cb34f321f501a88e5b0e
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-08 01:18:22 +0800

    Get element by class name

--- dom.js
+++ dom.js
@@ -1,2 +1,33 @@
 //Examine the document object //
+
+
 // console.dir(document); 
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[9]);
+// document.all[9].textContent = 'Hello World'; 
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// GET ELEMENTBY ID //
+
+// console.log(document.getElementById('header-title'));
+
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello World';
+// headerTitle.innerText = 'Goodbye';
+// headerTitle.innerHTML = '<h3>Hello World</h3>';
+
+// headerTitle.style.borderBottom = '2px solid';
+// header.style.borderBottom = '3px solid #000';
+
+// GET ELEMENTSBYCLASSNAME 22:54
