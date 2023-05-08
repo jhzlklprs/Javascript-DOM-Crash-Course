d001b244f8510f42224e38d30b4d17ce13d33d02
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-08 22:36:36 +0800

    Learning DOM part 1

--- dom.js
+++ dom.js
@@ -23,11 +23,85 @@
 //var headerTitle = document.getElementById('header-title');
 //var header = document.getElementById('main-header');
 // console.log(headerTitle);
-// headerTitle.textContent = 'Hello World';
-// headerTitle.innerText = 'Goodbye';
+// headerTitle.textContent = 'Hello World'; == focus on just text
+// headerTitle.innerText = 'Goodbye'; == affects style on text
 // headerTitle.innerHTML = '<h3>Hello World</h3>';
 
 // headerTitle.style.borderBottom = '2px solid';
 // header.style.borderBottom = '3px solid #000';
 
 // GET ELEMENTSBYCLASSNAME 22:54
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+// // Gives Error
+// //items.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < items.length; i++){
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+
+// GET ELEMENTSBYTAGNAME
+
+
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+// // Gives Error
+// //items.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < li.length; i++){
+//     li[i].style.backgroundColor = '#f4f4f4';
+// }
+
+
+
+// QUERYSELECTOR //
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = "SEND";
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'yellow';
+
+
+// QUERYSELECTORALL //
+
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+}
