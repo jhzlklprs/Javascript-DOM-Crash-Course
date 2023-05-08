662fcc2c74412c6200e0ea489c789e3aa648e42e
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-09 01:56:10 +0800

    DOM part 2

--- dom.js
+++ dom.js
@@ -92,16 +92,23 @@
 
 // QUERYSELECTORALL //
 
-var titles = document.querySelectorAll('.title');
+// var titles = document.querySelectorAll('.title');
 
-console.log(titles);
-titles[0].textContent = 'Hello';
+// console.log(titles);
+// titles[0].textContent = 'Hello';
 
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
 
-for(var i = 0; i < odd.length; i++){
-    odd[i].style.backgroundColor = '#f4f4f4';
-    even[i].style.backgroundColor = '#ccc';
-}
+// for(var i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = '#f4f4f4';
+//     even[i].style.backgroundColor = '#ccc';
+// }
+
+
+
+var itemList = document.querySelector('#items');
+// parentNode
+
+console.log(itemList.parentNode);
