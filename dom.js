1fb7a7b311074b28a4a6dc028477eb5bef8832fe
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-11 02:24:23 +0800

    Forms

--- dom.js
+++ dom.js
@@ -176,9 +176,67 @@ var itemList = document.querySelector('#items');
 // var container = document.querySelector('header .container');
 // var h1 = document.querySelector('header h1');
 
-
 // console.log(newDiv);
 
 // newDiv.style.fontSize = '30px';
 
 // container.insertBefore(newDiv, h1);
+
+//EVENTS
+
+// const button = document.getElementById('button').addEventListener('click', buttonClick);
+
+// function buttonClick(e){
+//     //console.log('Button clicked');
+//     // document.getElementById('header-title').textContent = 'Changed';
+//     //console.log(e);
+
+
+//     // console.log(e.target);
+//     // console.log(e.target.id);
+//     // console.log(e.target.className);
+//     // console.log(e.target.classList);
+//     // const output = document.getElementById('output');
+//     // output.innerHTML = '<h3>' +e.target.id+ '</h3>';
+
+//     // console.log(e.type);
+
+//     // console.log(e.clientX);
+//     // console.log(e.clientY);
+
+//     //console.log(e.offsetX);
+//     //console.log(e.offsetY);
+
+//     //console.log(e.altKey);
+//     //console.log(e.ctrlKey);
+//     //console.log(e.shiftKey);
+//}
+
+const button = document.getElementById('button');
+const box = document.getElementById('box');
+
+//button.addEventListener('click', runEvent);
+//button.addEventListener('dblclick', runEvent);
+//button.addEventListener('mousedown', runEvent);
+//button.addEventListener('mouseup', runEvent);
+
+//box.addEventListener('mouseenter', runEvent);
+//box.addEventListener('mouseleave', runEvent);
+
+//box.addEventListener('mouseover', runEvent);
+//box.addEventListener('mouseout', runEvent);
+
+
+//box.addEventListener('mousemove', runEvent);
+
+
+const itemInput = document.querySelector('input[type="text"]');
+const form = document.querySelector('form');
+
+function runEvent(e){
+    console.log('EVENT TPYE: ' +e.type);
+
+    // output.innerHTML = '<h3>MouseX: ' + e.offsetX+ '</h3><h3>MouseY: ' +e.offsetY + '</h3>'
+
+    // document.body.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 40)";
+}
